6df87ea1f9da1ad492a030d7f2ac40f3aa9ad58c
Barbara209762 <massoubarbara55@gmail.com>
2025-01-10 14:23:06 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,19 +1,7 @@
 import pandas as pd
 # Charger le fichier CSV
 data = pd.read_csv("data_dashboard_large.csv")
-# Conversion des dates
-data['Date_Transaction'] = pd.to_datetime(data['Date_Transaction'])
 
-import streamlit as st
-import plotly.express as px
-# Titre principal
-st.title("Dashboard Interactif : Performances de la chaîne de magasins")
-# Sidebar pour filtres
-with st.sidebar:
-    st.header("Filtres dynamiques")
-    magasins = st.multiselect("Sélectionnez les magasins", data['Magasin'].unique(), default=data['Magasin'].unique())
-    categories = st.multiselect("Sélectionnez les catégories de produit", data['Categorie_Produit'].unique(), default=data['Categorie_Produit'].unique())
-    date_range = st.date_input("Période", [data['Date_Transaction'].min(), data['Date_Transaction'].max()])
 
 
 
