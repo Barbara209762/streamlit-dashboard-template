43b6f047881481635a6beee3fe32e665eb21d87b
Barbara209762 <massoubarbara55@gmail.com>
2025-01-13 19:13:23 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ data = pd.read_csv("data_dashboard_large - data_dashboard_large.csv")
 import streamlit as st
 
 # Titre principal
-st.title("Dashboard Interactif : Performances de la chaîne de magasins",layout="wide")
+st.title("Dashboard Interactif : Performances de la chaîne de magasins")
 
 
 
