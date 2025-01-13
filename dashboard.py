f4c6221df001ab7467c582b88d21c6e879e21a12
Barbara209762 <massoubarbara55@gmail.com>
2025-01-13 12:04:53 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,7 @@ st.title("Dashboard Interactif : Performances de la chaîne de magasins")
 
 # Sidebar pour filtres
 with st.sidebar:
-st.header("Filtres dynamiques")
+
 magasins = st.multiselect("Sélectionnez les magasins", data['Magasin'].unique(), default=data['Magasin'].unique())
 categories = st.multiselect("Sélectionnez les catégories de produit", data['Categorie_Produit'].unique(), default=data['Categorie_Produit'].unique())
 date_range = st.date_input("Période", [data['Date_Transaction'].min(), data['Date_Transaction'].max()])
