3022887919951796cc40a3ff60bcadd433f9fbd2
Barbara209762 <massoubarbara55@gmail.com>
2025-01-13 19:32:50 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,8 +7,8 @@ import plotly.express as px
 # Titre principal
 st.title("Dashboard Interactif : Performances de la chaîne de magasins")
 # Sidebar pour filtres
-with st.sidebar:
-st.header("Filtres dynamiques")
+with st.container():   
+    st.header("Filtres dynamiques")
 magasins = st.multiselect("Sélectionnez les magasins", data['Magasin'].unique(), default=data['Magasin'].unique())
 categories = st.multiselect("Sélectionnez les catégories de produit", data['Categorie_Produit'].unique(), default=data['Categorie_Produit'].unique())
 date_range = st.date_input("Période", [data['Date_Transaction'].min(), data['Date_Transaction'].max()])
