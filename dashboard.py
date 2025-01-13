de2915ff9b98f5c58a1159e7a2915016919c7636
Barbara209762 <massoubarbara55@gmail.com>
2025-01-13 18:45:06 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,11 +12,20 @@ magasins = st.multiselect("Sélectionnez les magasins", data['Magasin'].unique()
 categories = st.multiselect("Sélectionnez les catégories de produit", data['Categorie_Produit'].unique(), default=data['Categorie_Produit'].unique())
 date_range = st.date_input("Période", [data['Date_Transaction'].min(), data['Date_Transaction'].max()])
 
-# Filtrage des données
-filtered_data = data[
-(data['Magasin'].isin(magasins)) &
-(data['Categorie_Produit'].isin(categories)) 
-]
+# Calculs des KPIs :
+total_sales = filtered_data['Montant'].sum()
+total_transactions = filtered_data.shape[0]
+avg_transaction = filtered_data['Montant'].mean()
+avg_satisfaction = filtered_data['Satisfaction_Client'].mean()
+# Affichage des KPIs
+st.subheader("Vue d'ensemble")
+st.metric("Total des ventes (€)", f"{total_sales:,.2f}")
+st.metric("Nombre total de transactions", total_transactions)
+st.metric("Montant moyen par transaction (€)", f"{avg_transaction:,.2f}")
+st.metric("Satisfaction client moyenne", f"{avg_satisfaction:.2f}")
+
+
+
 
 
 
