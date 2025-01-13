1582e980907323f0f8897480df54c126531662ff
Barbara209762 <massoubarbara55@gmail.com>
2025-01-13 19:29:12 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,14 +2,19 @@ import pandas as pd
 
 data = pd.read_csv("data_dashboard_large - data_dashboard_large.csv")
 import streamlit as st
+import plotly.express as px
 
 # Titre principal
 st.title("Dashboard Interactif : Performances de la chaîne de magasins")
-st.header("Vue d'ensemble")
-col1, col2, col3, col4 = st.columns(4)
+# Sidebar pour filtres
+with st.sidebar:
+st.header("Filtres dynamiques")
+magasins = st.multiselect("Sélectionnez les magasins", data['Magasin'].unique(), default=data['Magasin'].unique())
+categories = st.multiselect("Sélectionnez les catégories de produit", data['Categorie_Produit'].unique(), default=data['Categorie_Produit'].unique())
+date_range = st.date_input("Période", [data['Date_Transaction'].min(), data['Date_Transaction'].max()])
 
-st.subheader("Histogramme des ventes quotidiennes")
-st.bar_chart(ventes_quotidiennes, x="Date_Transaction", y="Montant")
+
+
 
 
 
