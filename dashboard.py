09d5cdee16c8b85eceb0ae36bc1cc4c44c9b6c10
Barbara209762 <massoubarbara55@gmail.com>
2025-01-13 19:35:08 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,10 +8,19 @@ import streamlit as st
 st.title("Dashboard Interactif : Performances de la chaîne de magasins")
 # Sidebar pour filtres
 with st.container():   
-    st.header("Filtres dynamiques")
+    st.header("1.Filtres dynamiques")
 magasins = st.multiselect("Sélectionnez les magasins", data['Magasin'].unique(), default=data['Magasin'].unique())
 categories = st.multiselect("Sélectionnez les catégories de produit", data['Categorie_Produit'].unique(), default=data['Categorie_Produit'].unique())
 date_range = st.date_input("Période", [data['Date_Transaction'].min(), data['Date_Transaction'].max()])
+
+# Filtrage des données
+filtered_data = data[
+    (data['Magasin'].isin(magasins)) &
+    (data['Categorie_Produit'].isin(categories)) &
+    (data['Date_Transaction'].between(date_range[0], date_range[1]))
+]
+
+
 
 
 
